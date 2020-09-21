55ce4a0e575f6074f417099d8d9e6d2be563b9f1
Ryan Kim <iawal86@gmail.com>
2020-09-20 20:49:19 -0700

    created schema

---
+++ data.js
@@ -0,0 +1,13 @@
+const data = [
+  {
+    url: "url",
+    channel: "channel",
+    song: "song name",
+    likes: "num of likes",
+    messages: "message",
+    description: "description",
+    shares: "num of shares",
+  },
+];
+
+export default data;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import Data from "./data.js";
 
 // app config
 const app = express();
@@ -11,6 +12,8 @@ const port = 8000;
 
 // api endpoint
 app.get("/", (req, res) => res.status(200).send("Hello World"));
+
+app.get("/api/posts", (req, res) => res.status(200).send(Data));
 
 // listener
 app.listen(port, () => console.log(`Listening on port: ${port}`));
